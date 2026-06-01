f77f86053a9eae3a036f7c0b33b384cc32317123
RenataHader <renata.hader273@gmail.com>
2026-06-01 22:18:21 +0200

    Initial commit

---
+++ src/main/java/com/example/budget/PersonalBudgetApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBudgetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBudgetApiApplication.class, args);
+	}
+
+}
