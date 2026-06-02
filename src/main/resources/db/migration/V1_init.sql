d1b7a15f145c41e9abf777eb4131311eacebcd9e
RenataHader <renata.hader273@gmail.com>
2026-06-02 18:48:20 +0200

    Add initial database migration

---
+++ src/main/resources/db/migration/V1_init.sql
@@ -0,0 +1,34 @@
+CREATE TABLE account(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHARE(255) NOT NULL,
+    balance NUMERIC(19, 2) NOT NULL DEFAULT 0
+);
+
+CREATE TABLE transactions(
+    id BIGSERIAL PRIMARY KEY,
+    amount NUMERIC(19, 2) NOT NULL
+    type VARCHAR(10) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    description TEXT,
+    transaction_date DATE NOT NULL,
+    account_id BIGINT NOT NULL,
+
+    CONSTRAINT fk_transactions_account
+        FOREIGN KEY (account_id)
+        REFERENCES accounts(id),
+
+    CONSTRAINT chk_transaction_amount_positive
+        CHECK (amount > 0),
+
+    CONSTRAINT chk_transaction_type
+        CHECK (type IN ('INCOME', 'EXPENSE'))
+);
+
+CREATE INDEX idx_transactions_account_id
+    ON transactions(account_id);
+
+CREATE INDEX idx_transactions_transaction_date
+    ON transactions(transaction_date);
+
+CREATE INDEX idx_transactions_category
+    ON transactions(category);
